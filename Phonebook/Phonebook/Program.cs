591442c4dda1dedc6bf382094e9665c7e22e78da
unknown <kkostova59@gmail.com>
2019-09-10 12:57:16 +0300

    Added ListAll

--- Phonebook/Phonebook/Program.cs
+++ Phonebook/Phonebook/Program.cs
@@ -19,20 +19,26 @@ namespace Phonebook
                 if (input[0] == "End" || input[0] == "END")
                     break;
 
-                string name = input[1];
+                //string name = input[1];
                 switch (input[0])
                 {
                     case "A":
-                        if (phonebook.ContainsKey(name))
+                        if (phonebook.ContainsKey(input[1]))
                             Console.WriteLine("The name already exist.");
                         else
-                            phonebook.Add(name, input[2]);
+                            phonebook.Add(input[1], input[2]);
                         break;
                     case "S":
-                        if (phonebook.ContainsKey(name))
-                            Console.WriteLine($"{name} -> {phonebook[name]}");
+                        if (phonebook.ContainsKey(input[1]))
+                            Console.WriteLine($"{input[1]} -> {phonebook[input[1]]}");
                         else
-                            Console.WriteLine($"Contact {name} does not exist.");
+                            Console.WriteLine($"Contact {input[1]} does not exist.");
+                        break;
+                    case "ListAll":
+                        foreach (KeyValuePair<string, string> item in phonebook)
+                        {
+                            Console.WriteLine($"{item.Key} -> {item.Value}");
+                        }
                         break;
                     default:
                         Console.WriteLine("The commands are A for Add and S for Search");

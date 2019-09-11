dfb2aade5d934aa92fdc6de5945b8db9c9c0152c
unknown <kkostova59@gmail.com>
2019-09-11 17:00:55 +0300

    Initial commit

---
+++ HomeworkTelerik/StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Something";
+            string result = str.SubstringTwo(1,4);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ HomeworkTelerik/StringBuilder/StringBuilder/StringBuilder.cs
@@ -0,0 +1,23 @@
+﻿namespace StringBuilder
+{
+    public static class StringBuilder
+    {
+        public static string SubstringTwo(this string input, int index, int length)
+        {
+            if (index < 0 || index > input.Length)
+                return "Index must be over 0 and smaller than the count of letters.";
+            else if (length < 0 || length > input.Length + index)
+                return "Length must be over 0 and smaller than the count of letters.";
+            else
+            {
+                char[] inputArray = input.ToCharArray();
+                char[] result = new char[length];
+                for (int i = 0; i < length; i++)
+                {
+                    result[i] = inputArray[index+i];
+                }
+                return new string(result);
+            }
+        }
+    }
+}

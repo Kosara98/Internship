0e8ecaa82287eea4c25a7732d424430aa1a05f4f
unknown <kkostova59@gmail.com>
2019-09-12 13:39:22 +0300

    Added Problem 5 : Order students

--- HomeworkTelerik/FirstBeforeLast/FirstBeforeLast/Program.cs
+++ HomeworkTelerik/FirstBeforeLast/FirstBeforeLast/Program.cs
@@ -18,6 +18,7 @@ namespace FirstBeforeLast
                 new Student("Aaaaaa","YYYY")
             };
 
+            //Problem 3: First before Last
             var result = FirstNameBeforeLastName(students);
             var resultLambda = students.Where(x => x.FirstName[0] < x.LastName[0]).ToArray();
 
@@ -26,7 +27,19 @@ namespace FirstBeforeLast
 
             foreach (var item in resultLambda)
                 Console.WriteLine(item.FirstName);
-            
+
+            //Problem 5: Order students
+            var orderStudentsLambda = students.OrderByDescending(x => x.FirstName).ThenBy(x => x.LastName);
+            var orderStudents = from student in students
+                                orderby student.FirstName descending, student.LastName 
+                                select student;
+
+            foreach (var item in orderStudentsLambda)
+                Console.WriteLine(item.FirstName);
+            foreach (var item in orderStudents)
+                Console.WriteLine(item.FirstName);
+
+
         }
         public static IEnumerable<Student> FirstNameBeforeLastName(IEnumerable<Student> students)
         {

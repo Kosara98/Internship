4c3a83d28db6829c3fc50dcc10d636c7c2a77df9
unknown <kkostova59@gmail.com>
2019-09-12 09:39:42 +0300

    Added Problem 2: IEnumerable extensions

---
+++ HomeworkTelerik/IEnumerableExtensions/IEnumerableExtensions/IEnumerbleExtensionss.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IEnumerableExtensions
+{
+    public static class IEnumerbleExtensionss
+    {
+        public static T Sum<T>(this IEnumerable<T> numbers)
+        {
+            T result = (dynamic)0;
+            CheckingIfTheListIsEmpty(numbers);
+
+            foreach (var item in numbers)
+                result += (dynamic)item;
+
+            return result;
+        }
+        
+        public static double Product<T>(this IEnumerable<T> numbers)
+        {
+            CheckingIfTheListIsEmpty(numbers);
+            double result = 1;
+
+            foreach (var item in numbers)
+                result *= (dynamic)item;
+
+            return result;
+        }
+
+        public static T Min<T>(this IEnumerable<T> numbers)
+        {
+            CheckingIfTheListIsEmpty(numbers);
+            T result = numbers.First();
+
+            foreach (var item in numbers)
+                if (result > (dynamic)item)
+                    result = item;
+
+            return result;
+        }
+
+        public static T Max<T>(this IEnumerable<T> numbers)
+        {
+            CheckingIfTheListIsEmpty(numbers);
+            T result = (dynamic)(-1);
+
+            foreach (var item in numbers)
+                if (result < (dynamic)item)
+                    result = item;
+
+            return result;
+        }
+
+        public static double Average<T>(this IEnumerable<T> numbers)
+        {
+            CheckingIfTheListIsEmpty(numbers);
+
+            double result = (dynamic)numbers.Sum();
+            result = result / numbers.Count();
+            return result;
+        }
+
+        private static void CheckingIfTheListIsEmpty<T>(IEnumerable<T> numbers)
+        {
+            if (numbers.Count() == 0)
+                throw new ArgumentException("The list/array is empty");
+        }
+    }
+}
---
+++ HomeworkTelerik/IEnumerableExtensions/IEnumerableExtensions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerableExtensions
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>();
+            for (int i = 10; i > 2; i--)
+            {
+                nums.Add(i);
+            }
+
+            Console.WriteLine("The sum is: " + nums.Sum());
+            Console.WriteLine("The avarege is: " + nums.Average());
+            Console.WriteLine("The max is: " + nums.Max());
+            Console.WriteLine("The min is: " + nums.Min());
+            Console.WriteLine("The product is: " +  nums.Product());
+        }
+    }
+}

f263dd09454c640634380154dac4b2df9c5f9496
unknown <kkostova59@gmail.com>
2019-09-12 14:00:45 +0300

    Added Problem 6 : Divisible by 7 and 3

---
+++ HomeworkTelerik/DivisibleBy7And3/DivisibleBy7And3/DivisibleExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DivisibleBy7And3
+{
+    public static class DivisibleExtensions
+    {
+        public static void DivideBy7And3(this IEnumerable<int> numbers)
+        {
+            var result = from num in numbers
+                         where num % 7 == 0 && num % 3 == 0
+                         select num;
+
+            foreach (var item in result)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ HomeworkTelerik/DivisibleBy7And3/DivisibleBy7And3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisibleBy7And3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 3, 21, 42 };
+            numbers.DivideBy7And3();
+
+            var result = numbers.Where(x => x % 7 == 0 && x % 3 == 0);
+            foreach (var item in result)
+                Console.WriteLine(item);
+        }
+    }
+}

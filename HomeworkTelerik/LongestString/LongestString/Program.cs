df394989483194e5adc75b21462aa0ef91599dd9
unknown <kkostova59@gmail.com>
2019-09-12 17:04:13 +0300

    Added Problem 17: Longest String

---
+++ HomeworkTelerik/LongestString/LongestString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "aa", "aaaa", "dassadasd" };
+
+            var maxLength = (from word in words
+                            orderby word.Length
+                            select word).Last();
+
+            Console.WriteLine(maxLength);
+        }
+    }
+}

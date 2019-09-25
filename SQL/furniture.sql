1407f49794d9d6fc1ac4bc7d273ffb5b6669b68b
unknown <kkostova59@gmail.com>
2019-09-25 14:58:16 +0300

    Added new table: ProductSales

--- SQL/furniture.sql
+++ SQL/furniture.sql
@@ -21,11 +21,16 @@ create table Clients(
 create table Sales(
 	Id int identity(1,1) not null primary key,
 	SaleDate date not null,
-	ProductId int not null references Products(Id),
 	Quantity int not null,
 	ClientId int not null references Clients(Id),
 	Invoice varchar(10) not null,
 	Price decimal(18,2) not null
+)
+
+create table ProductSales(
+	ProductId int not null references Products(Id),
+	SaleId int not null references Sales(Id),
+	primary key(ProductId, SaleId)
 )
 
 insert into Products
@@ -50,16 +55,31 @@ insert into Clients
 values('BRR OOD','bul. "Tsar Boris III Obedinitel" 31', 112233789, 1,'Denislav Todorov')
 
 insert into Sales
-values (convert(datetime, '18-09-2019',105), 2, 3, 1, 12345412, 27)
+values (convert(datetime, '18-09-2019',105), 3, 1, 12345412, 27)
 
 insert into Sales
-values (convert(datetime, '20-08-2019',105), 1, 2, 1, 48545412, 201)
+values (convert(datetime, '20-08-2019',105), 2, 1, 48545412, 201)
 
 insert into Sales
-values (convert(datetime, '15-05-2019',105), 4, 3, 2, 25254412, 200)
+values (convert(datetime, '15-05-2019',105), 3, 2, 25254412, 200)
 
 insert into Sales
-values (convert(datetime, '01-07-2019',105), 3, 1, 3, 65658412, 78)
+values (convert(datetime, '01-07-2019',105), 1, 3, 65658412, 78)
+
+insert into ProductSales
+values (1,1)
+
+insert into ProductSales
+values (2,1)
+
+insert into ProductSales
+values (1,3)
+
+insert into ProductSales
+values (3,2)
+
+insert into ProductSales
+values (2,2)
 
 /* search product by part of the name*/
 
@@ -68,13 +88,15 @@ where Name like '%ed'
 
 /* search product and their price and buyer by bill*/
 
-select pr.Name as ProductName, pr.Price, cs.Name as ClientName
-from Products pr join (select c.Name, s.ProductId, s.Invoice
-						from Clients c join Sales s
-						on c.Id = s.ClientId) as cs
-on pr.Id = cs.ProductId
-where cs.Invoice = 12345412
-
+select b.Name, b.Price, c.Name
+from Clients c join (select a.Name, a.Price, s.ClientId, s.Invoice
+					from Sales s join (select pr.Name,pr.Id, pr.Price, ps.SaleId
+										from Products pr join ProductSales ps
+										on pr.Id = ps.ProductId) as a
+					on s.Id = a.SaleId) as b
+on c.Id = b.ClientId
+where b.Invoice = 48545412
+			
 /* name of the company that bought something for the last month*/
 
 select c.Name as CompanyName
@@ -84,7 +106,9 @@ where s.SaleDate > '2019-08-18'
 
 /* quantity of every product that is bought for the last one month*/
 
-select pr.Name as ProductName, s.Quantity 
-from Products pr join Sales s
-on pr.Id = s.ProductId
+select s.Quantity, a.Name
+from Sales s join (select pr.Name, ps.SaleId
+					from Products pr join ProductSales ps
+					on pr.Id = ps.ProductId) as a
+on s.Id = a.SaleId
 where s.SaleDate > '2019-08-18'

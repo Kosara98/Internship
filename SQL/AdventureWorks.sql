1796b5aaf5e3be208e58a9d0eb61be1617e672e7
unknown <kkostova59@gmail.com>
2019-09-30 10:49:32 +0300

    Fixed dates and 'Order number and the next one' query

--- SQL/AdventureWorks.sql
+++ SQL/AdventureWorks.sql
@@ -4,7 +4,7 @@ select distinct sh.SalesOrderNumber, sh.OrderDate, p.Name
 from SalesLT.SalesOrderHeader sh
 join SalesLT.SalesOrderDetail sd on sh.SalesOrderId = sd.SalesOrderID
 join SalesLT.Product p on sd.ProductID = p.ProductID
-where OrderDate >= '2011-10-01' and OrderDate < '2011-10-02'
+where OrderDate = '20111001'
 
 /*The order numbers and the quantity of products made on 1.10.2011*/
 
@@ -12,8 +12,9 @@ select distinct sh.SalesOrderNumber, count(sd.SalesOrderId)
 from SalesLT.SalesOrderHeader sh
 join SalesLT.SalesOrderDetail sd on sh.SalesOrderId = sd.SalesOrderID
 join SalesLT.Product p on sd.ProductID = p.ProductID
-where OrderDate >= '2011-10-01' and OrderDate < '2011-10-02'
+where OrderDate = '20111001'
 group by sh.SalesOrderNumber
+
 
 /*Order numbers made on 1.10.2011 with quantity of products between 3 and 9*/
 
@@ -21,22 +22,22 @@ select distinct sh.SalesOrderNumber, count(sd.SalesOrderId)
 from SalesLT.SalesOrderHeader sh
 join SalesLT.SalesOrderDetail sd on sh.SalesOrderId = sd.SalesOrderID
 join SalesLT.Product p on sd.ProductID = p.ProductID
-where OrderDate >= '2011-10-01' and OrderDate < '2011-10-02'
+where OrderDate = '20111001'
 group by sh.SalesOrderNumber
 having count(sd.SalesOrderId) between 3 and 9
 
 /*Order number and the next one*/
 
-select SalesOrderNumber, lead(SalesOrderNumber, 1) 
-over(order by SalesOrderNumber) as NextOrderNumber
+select SalesOrderNumber, concat('SO', SalesOrderID + 1) as NextSalesOrderNumber
 from SalesLT.SalesOrderHeader
+where SalesOrderNumber = 'SO71946'
 
 /*The names of clients made purchase before 30.09.2012 and after 30.06.2013*/
 
 select concat(c.FirstName,' ', c.LastName) as Name, sh.OrderDate
 from SalesLT.Customer c 
 join SalesLT.SalesOrderHeader sh on sh.CustomerID = c.CustomerID
-where sh.OrderDate < '2012-09-30' or sh.OrderDate > '2013-06-30'
+where sh.OrderDate < '20120930' or sh.OrderDate > '20130630'
 
 /*List of order number and the order number before and after with the dates of Jeffrey Kurtz*/
 
@@ -48,3 +49,4 @@ lag(OrderDate, 1) over(order by SalesOrderNumber) as PreviousOrderDate
 from SalesLT.Customer c 
 join SalesLT.SalesOrderHeader sh on sh.CustomerID = c.CustomerID
 where c.FirstName = 'Jeffrey' and c.LastName = 'Kurtz'
+
